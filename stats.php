dda5e12e93555d619098ebac6320b96114371b67
ZIANI SHAMSDINE p1718423 <shamsdine.ziani@etu.univ-lyon1.fr>
2020-04-20 17:21:13 +0200

    stats

--- stats.php
+++ stats.php
@@ -18,6 +18,7 @@ $pdo = getConnection();
 
 <head>
     <?php include("includes/head-tag-contents.php"); ?>
+    <link rel="stylesheet" href="styles/stats.css">
 </head>
 
 <body class="bg-light">
@@ -25,8 +26,9 @@ $pdo = getConnection();
 
     <div class="card container bg-white" id="main-content">
         <h2>Statistiques du site</h2>
-        <?php echo '<h4>Il y a: '.getUserCount($pdo).' inscrits sur Pic !</h4>'; ?>
-        <h3>Nombre de photos par utilisateurs<h3>
+        <?php echo '<h4> Il y a: <red>'.getUserCount($pdo).'</red> inscrits sur Pic !</red></h4>'; 
+              echo '<h4>Au total, <gray>'.getPicturesCount($pdo).'</gray> photos ont été postées sur Pic !</h4>';?>
+        
     </div>
 
     <?php include("includes/footer.php"); ?>
